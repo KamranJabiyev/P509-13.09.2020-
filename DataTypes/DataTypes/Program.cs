2805dd8e79b459391ba76c1ee80f9d2febba8efd
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-09-13 22:20:07 +0400

    Add files via upload

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte age = 255;
+            //age = 200;
+            //sbyte degree = -100;
+            //char letter = 'a';
+            //letter = '1';
+            //string name = "Rufet";
+            //name = "Test";
+            //decimal pi = 3.19M;
+            //Console.WriteLine(pi);
+
+            //int a = 10;
+
+            //Console.WriteLine(a++);
+            //Console.WriteLine(a++);
+            //Console.WriteLine(a);
+            //Console.WriteLine(a);
+            //a = 16;
+            //Console.WriteLine(a);
+
+            //int x = 10;
+
+            //x += 5;  //x=x+5
+            //x -= 5;
+            //x /= 2;
+            //x *= 2;
+            //x %= 3;
+            //Console.WriteLine(x);
+
+            //int a = 11;
+            //int b = 11;
+            //string letter = "aaa";
+            //Console.WriteLine(a==b);
+            //Console.WriteLine(a != b);
+            //Console.WriteLine(a>=b);
+
+            //int age = 20;
+
+            //string result = (age >= 16) ? "Xosh gelmishsiniz" : "Qadagandir";
+            //Console.WriteLine(result);
+
+            //if (age >= 16)
+            //{
+            //    Console.WriteLine("Xosh gelmishsiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Qadagandir");
+            //}
+
+            //string city = "Gence";
+            //int age = 15;
+
+            //Console.WriteLine(city=="Baki" && age>18);
+            //Console.WriteLine(!(city=="Baki" || age>18));
+
+            //int i = 1;
+
+            //while (i < 1)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //} while (i < 1);
+
+            //while (i <= 10)
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //}
+
+            //do
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //} while (i <= 10);
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int j = 10; j > 0; j--)
+            //{
+            //    Console.WriteLine(j);
+            //}
+        }
+    }
+}
